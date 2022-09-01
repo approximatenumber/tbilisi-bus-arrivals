98b74faefcb17255106446410d48b12fb35e2d94
Maksim <maksim.melnikov@arrival.com>
2022-09-01 15:23:02 +0400

    init

---
+++ tbilisi_arrivals.py
@@ -0,0 +1,32 @@
+from typing import List
+import requests
+from flask import Flask, render_template, request
+from unidecode import unidecode
+
+
+URL = "http://transfer.ttc.com.ge:8080/otp/routers/ttc/stopArrivalTimes"
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index() -> str:
+    if request.method == 'POST':
+        if request.form.get('stop_id') and request.form.get('get_schedule'):
+            if request.form.get('stop_id').isdigit():
+                return render_template('arrival.html', items=get_schedule(request.form.get('stop_id')))
+    return render_template('index.html')
+
+def get_schedule(stop_id: str) -> List:
+    r = requests.get(f"{URL}?stopId={stop_id}")
+    schedule = []
+    print(r.json())
+    for bus in r.json()['ArrivalTime']:
+        schedule.append(
+            {
+                'bus': bus['RouteNumber'],
+                'time': bus['ArrivalTime'],
+                'destination':unidecode(bus['DestinationStopName'])
+            }
+        )
+    print(schedule)
+    return schedule
